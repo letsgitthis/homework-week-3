f815279aa305e8d97ed68bd3a404acd40a09a513
John Bentley <jbentley1011@gmail.com>
2020-05-06 02:22:30 -0500

    current progress

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+
+let x = "0123456789!@#$%^&*()_-+=[];:./?QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+
+
+
+
+
+
+
+
+function hasNumbers (password) {}
+function hasUppercase () {}
+function hasLowercase () {}
+function hasNumbers () {}
+function hasSpecialCharacters () {}
+
+function getNumbers () {}
+function getLowercase () {}
+
+function generatePassword () {}
